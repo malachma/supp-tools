fefdb0970a43eedad7e4d124a74c5ba157662f7c
malachma <malachma@microsoft.com>
2018-06-14 17:31:13 +0200

    Added functionality to collect from any agent in the cluster

--- k8s/report.sh
+++ k8s/report.sh
@@ -1,18 +1,29 @@
 #/bin/bash
 
+#Delete the POD node-report. Be sure we have a clean environment
+kubectl delete pod -l app=node-report
+
+#Create a tempdir and move into it
+TDIR=$(mktemp -d /tmp/aks.XXXXXXXXX)
+cd $TDIR
+
 #create the POD node-report
 #It has root access to the agent-vm
-cat <<END | kubectl create -f -
+for agentname in $(kubectl get nodes -o jsonpath={.items[*].metadata.name}); do 
+cat <<END | kubectl create -f - ;
 apiVersion: v1
 kind: Pod
 metadata:
-  name: node-report
+  name: node-report-$agentname
   labels:
     app: node-report
 spec:
   hostNetwork: true
+  nodeSelector:
+    kubernetes.io/hostname: $agentname
   hostPID: true
   hostIPC: true
+
   containers:
   - name: alpine
     image: alpine:latest
@@ -28,33 +39,38 @@ spec:
     hostPath:
       path: /
 END
+
 #Sleep for 5 seconds to get the POD deployed
 sleep 5
 
 #Run the sosreport tool
 #Since we use chroot, sosreport installation is not needed. It is is already installed by default on every agent-vm
-TDIR=$(mktemp -d /tmp/aks.XXXXXXXXX)
-cd $TDIR
 
 echo "Creating the sosreport"
 echo "Please be patient ..."
-kubectl exec -t node-report -- chroot /agent-root sosreport -a --batch | grep -A 1 "Your sosreport has been generated and saved in:"  > batch.out
-kubectl cp node-report:/agent-root$(sed -e '1d; s/\s*//' batch.out) .
+kubectl exec -t node-report-$agentname -- chroot /agent-root sosreport -a --batch | grep -A 1 "Your sosreport has been generated and saved in:"  > batch-$agentname.out
+kubectl cp node-report-$agentname:/agent-root$(sed -e '1d; s/\s*//' batch-$agentname.out) .
 
 #Collect the container logs
 echo "Collecting the container-logs"
-kubectl exec -t node-report -- chroot /agent-root tar chf /tmp/container_logs.tar /var/log/containers
-kubectl cp node-report:/agent-root/tmp/container_logs.tar .
+kubectl exec -t node-report-$agentname -- chroot /agent-root tar chf /tmp/container_logs.tar /var/log/containers
+kubectl cp node-report-$agentname:/agent-root/tmp/container_logs.tar container-$agentname-logs.tar
+
+#Get the kublet log. As it is not a POD/Container anymore with 1.9.6 it can only be fetched via journalctl
+kubectl exec -t node-report-$agentname -- chroot /agent-root journalctl -u kubelet --no-pager > kubelet-$agentname.log
 
 #Remove the batch.out file
-rm batch.out
+rm batch-$agentname.out
+#End the for loop
+done
+
 #Create a tar file and add the sosreport and the container_logs to it 
 tar cf /tmp/aks-report.tar *
-
+echo "The created aks-report is located in /tmp. the name of the archive is aks-report.tar"
+echo "Please pass over the tar archive to the support engineer"
 #Delete the tmp-dir
 cd /tmp
 rm -fr $TDIR
 
 #Delete the POD node-report
-kubectl delete pod node-report
-
+kubectl delete pod -l app=node-report
